06d124f2ff7dea014dde69f831b1f123600d6928
ZirjyZakwanFayyadhiya <zirjyzakwan@gmail.com>
2023-02-24 21:50:03 +0700

    Selesai Cuy

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/siswa';
+    public const HOME = '/siswas';
 
     /**
      * The controller namespace for the application.
